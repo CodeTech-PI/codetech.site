2cc27a323cbfff728d40bde30eca449e251b70f6
gabrieloliveira0505 <gabriel.oliveira165@sptech.school>
2024-10-01 17:49:35 -0300

    migrando front

---
+++ code-front/src/componentes/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+
+
+        <section>
+
+            <nav className='nav-bar'>
+
+                <img src="/imagens/logo.png"
+                    alt="Logo do estudico com com duas imagens de dois cup cake e um máquina de tatuagem no meio"></img>
+                <ul className='teste'>
+                    <li >Guerreiras</li>
+                    <li>Cuidados</li>
+                    <li>Serviços</li>
+                    <li>Anaminse</li>
+                    <li>Login</li>
+                </ul>
+
+            </nav>
+
+            <div className='linha'></div>
+
+        </section>
+
+
+    );
+}
+
+export default NavBar
