43206b40313e0aa2d5f456cbec49182e28e5292a
florencionath <nathalia.pinto@sptech.school>
2024-10-28 20:23:59 -0300

    ajuste em estilização

--- code-front/src/components/IconesContato/IconesContato.js
+++ code-front/src/components/IconesContato/IconesContato.js
@@ -4,7 +4,7 @@ const IconesContato = (props) => {
     return(
     
         <a href={props.link} target="_blank" rel="noopener noreferrer">
-        <img className='icones-contato' src={props.img} descricao={props.desc}></img>
+        <img className='icones-contato' src={props.img} alt={props.desc}></img>
         </a>
        
     )
