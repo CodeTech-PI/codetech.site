4f39137eeca526cff0c619295c8cf4f5b6f4deca
gabrieloliveira0505 <gabriel.oliveira165@sptech.school>
2024-11-26 17:20:44 -0300

    ajustando tela de atendimento

--- code-front/src/pages/Atendimento/Atendimento.js
+++ code-front/src/pages/Atendimento/Atendimento.js
@@ -3,6 +3,7 @@ import { Modal, Button, Form, ProgressBar, Alert } from 'react-bootstrap';
 import styled from 'styled-components';
 import agendamentoService from '../../services/agendamentoService';
 import clienteService from '../../services/clienteService';
+import listaProdutosService from "../../services/listaProdutosService";
 
 // Estilizando o Modal usando styled-components
 const StyledModal = styled(Modal)`
@@ -20,6 +21,23 @@ const StepContainer = styled.div`
 const CustomButton = styled(Button)`
   margin: 0 5px;
 `;
+const ProductBox = styled.div`
+  padding: 10px;
+  margin: 5px;
+  border: 1px solid #ccc;
+  background-color: #f9f9f9;
+  display: inline-block;
+  width: 45%;
+  text-align: left;
+`;
+
+const ProductList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+
 
 const Atendimento = () => {
     const [show, setShow] = useState(true); // Modal aparecerá ao carregar a página
@@ -40,11 +58,13 @@ const Atendimento = () => {
     const [error, setError] = useState(null); // Para exibir mensagens de erro
     const [step, setStep] = useState(1); // Adicionado o estado para controlar o passo do formulário
 
+    const [listaProdutos, setListaProdutos] = useState([]);
+    const [busca, setBusca] = useState('');
+    const [selectedProdutos, setSelectedProdutos] = useState([]); // Estado para armazenar os produtos selecionados
+
     useEffect(() => {
-        clienteService.getClientes() // Fazendo a chamada para a API
+        clienteService.getClientes()
             .then((response) => {
-                console.log(response, "Estrutura de dados da API"); // Verifique a estrutura
-                // Verifique se a resposta é um array
                 if (Array.isArray(response)) {
                     const usuariosDados = response.map(usuario => ({
                         id: usuario.id,
@@ -55,19 +75,28 @@ const Atendimento = () => {
                         dataNascimento: usuario.dataNascimento
                     }));
                     setUsuarios(usuariosDados);
-                    console.log(usuariosDados, "Usuários mapeados");
                 } else {
-                    console.log("Resposta não contém um array de usuários.");
                     setError("Resposta não contém um array de usuários.");
                 }
             })
             .catch((error) => {
-                console.error("Erro ao carregar usuários:", error);
                 setError("Erro ao buscar usuários.");
             });
     }, []);
-    
-    
+
+    useEffect(() => {
+        fetchListaProdutos(); // Buscar lista de produtos ao carregar o componente
+    }, []);
+
+    const fetchListaProdutos = async () => {
+        try {
+            const produtosData = await listaProdutosService.getListaProdutos();
+            setListaProdutos(produtosData);
+        } catch (error) {
+            console.error('Erro ao buscar lista de produtos', error);
+        }
+    };
+
     const handleClose = () => setShow(false);
 
     const handleInputChange = (e) => {
@@ -78,32 +107,9 @@ const Atendimento = () => {
         });
     };
 
-    const handleNestedInputChange = (e, category, field) => {
-        const { value } = e.target;
-        setFormData({
-            ...formData,
-            [category]: {
-                ...formData[category],
-                [field]: value
-            }
-        });
-    };
-
     const handleUserSelect = (e) => {
-        debugger
-        const selectedId = e.target.value;  // Pegando o id do usuário selecionado
-        console.log('ID Selecionado:', selectedId);  // Verifique no console se o id está correto
-
-        // Encontrando o usuário no array 'usuarios'
+        const selectedId = e.target.value;
         const usuario = usuarios.find(user => user.id === parseInt(selectedId));
-
-        if (usuario) {
-            console.log('Usuário Selecionado:', usuario);  // Verifique se o usuário foi encontrado
-        } else {
-            console.log('Usuário não encontrado!');
-        }
-
-        // Atualizando o estado com o usuário selecionado
         setSelectedUsuario(usuario);
         setFormData({
             ...formData,
@@ -113,35 +119,25 @@ const Atendimento = () => {
 
     const handlePostAgendamento = async () => {
         const { dt, cancelado } = formData;
-    
-        // Separando data e hora
         const [date, time] = dt.split('T');
         const [hour, minute] = time ? time.split(':') : ['00', '00'];
-    
-        // Concatenando a hora e minuto para enviar como uma string
-        const horario = `${hour}:${minute}:00`; // Se precisar do formato 'HH:mm:ss'
-    
-        console.log("Data e Hora Enviada:", { dt: date, horario }); // Verificando os dados
-    
-        // Estrutura do agendamento
+        const horario = `${hour}:${minute}:00`;
+
         const agendamentoData = {
             ...formData,
-            dt: date, // Apenas a data
-            horario: horario // Enviar o horário como string
+            dt: date,
+            horario: horario
         };
-    
-        // Se não estiver cancelado, incluir os dados do usuário
+
         if (!cancelado && selectedUsuario) {
             agendamentoData.usuario = {
                 ...selectedUsuario,
-                cpf: formData.usuario.cpf, // Adicionando o CPF informado
+                cpf: formData.usuario.cpf,
             };
         }
-    
+
         try {
-            debugger
             const response = await agendamentoService.postAgendamento(agendamentoData);
-            
             if (response) {
                 setStep(2); // Avançar para o próximo passo
             } else {
@@ -151,12 +147,30 @@ const Atendimento = () => {
             setError("Ocorreu um erro ao tentar agendar. Tente novamente.");
         }
     };
-    
-    
-    
+
+    const handleBusca = (e) => {
+        const query = e.target.value;
+        setBusca(query);
+    };
+
+    const handleProductSelect = (e) => {
+        const { value, checked } = e.target;
+        setSelectedProdutos((prevSelected) => {
+            if (checked) {
+                return [...prevSelected, value]; // Adiciona o produto ao array de selecionados
+            } else {
+                return prevSelected.filter((id) => id !== value); // Remove o produto do array de selecionados
+            }
+        });
+    };
 
     const nextStep = () => {
-        handlePostAgendamento(); // Chama o POST para o primeiro passo
+        if (step === 1) {
+            handlePostAgendamento(); // Chama o POST apenas no primeiro passo
+        } else if (step === 2) {
+            // No Step 2, apenas avança para o próximo passo sem chamar o POST
+            setStep(3); // Exemplo: Avançando para um próximo passo
+        }
     };
 
     const prevStep = () => {
@@ -164,16 +178,15 @@ const Atendimento = () => {
     };
 
     return (
-        <>
-            <StyledModal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Atendimento</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <ProgressBar now={25} label={`Step ${step} of 4`} />
+        <StyledModal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Atendimento</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <ProgressBar now={(step / 4) * 100} label={`Step ${step} of 4`} />
+                {error && <Alert variant="danger">{error}</Alert>} {/* Exibir erro, se houver */}
 
-                    {error && <Alert variant="danger">{error}</Alert>} {/* Exibir erro, se houver */}
-
+                {step === 1 && (
                     <StepContainer>
                         <h5>Data e Horário</h5>
                         <Form.Group>
@@ -185,49 +198,110 @@ const Atendimento = () => {
                                 onChange={handleInputChange}
                             />
                         </Form.Group>
-
+                        <Form.Group controlId="cancelado">
+                            <Form.Check
+                                type="checkbox"
+                                label="Cancelado"
+                                name="cancelado"
+                                checked={formData.cancelado}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
+                        <h5>Cliente</h5>
                         <Form.Group>
-                            <Form.Label>Selecionar Usuário</Form.Label>
-                            <Form.Control
-                                as="select"
-                                onChange={handleUserSelect}  // Chamando a função de seleção
-                                value={formData.usuario.id || ''}  // Valor do id do usuário selecionado
-                            >
-                                <option value="">Selecione um usuário</option>
-                                {Array.isArray(usuarios) && usuarios.length > 0 ? (
-                                    usuarios.map(usuario => (
-                                        <option key={usuario.id} value={usuario.id}>
-                                            {usuario.nome}  {/* Exibindo o nome do usuário */}
-                                        </option>
-                                    ))
-                                ) : (
-                                    <option value="">Nenhum usuário disponível</option>
-                                )}
+                            <Form.Label>Selecione o Cliente</Form.Label>
+                            <Form.Control as="select" onChange={handleUserSelect}>
+                                <option value="">Selecione</option>
+                                {usuarios.map((usuario) => (
+                                    <option key={usuario.id} value={usuario.id}>
+                                        {usuario.nome}
+                                    </option>
+                                ))}
                             </Form.Control>
                         </Form.Group>
+                        {selectedUsuario && (
+                            <>
+                                <Form.Group>
+                                    <Form.Label>Nome</Form.Label>
+                                    <Form.Control type="text" value={selectedUsuario.nome} readOnly />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Telefone</Form.Label>
+                                    <Form.Control type="text" value={selectedUsuario.telefone} readOnly />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" value={selectedUsuario.email} readOnly />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>CPF</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        value={formData.usuario.cpf}
+                                        onChange={(e) => handleInputChange(e, "usuario", "cpf")}
+                                    />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Data de Nascimento</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                        value={selectedUsuario.dataNascimento}
+                                        readOnly
+                                    />
+                                </Form.Group>
+                            </>
+                        )}
+                        <CustomButton variant="primary" onClick={nextStep}>
+                            Avançar
+                        </CustomButton>
+                    </StepContainer>
+                )}
 
-                        {/* <Form.Group>
-                            <Form.Label>CPF</Form.Label>
+                {step === 2 && (
+                    <StepContainer>
+                        <h5>Selecione o Produto</h5>
+                        <Form.Group>
+                            <Form.Label>Buscar Produto</Form.Label>
                             <Form.Control
                                 type="text"
-                                name="cpf"
-                                value={formData.usuario.cpf}
-                                onChange={(e) => handleNestedInputChange(e, 'usuario', 'cpf')}
-                                placeholder="Informe o CPF"
+                                value={busca}
+                                onChange={handleBusca}
+                                placeholder="Digite o nome do produto"
                             />
-                        </Form.Group> */}
+                        </Form.Group>
+
+                        <div className="product-list">
+    {listaProdutos
+        .filter((item) =>
+            item.produto.nome &&
+            item.produto.nome.toLowerCase().includes(busca.toLowerCase())
+        )
+        .map((item) => (
+            <div key={item.produto.id} className="product-box">
+                <Form.Check
+                    type="checkbox"
+                    value={item.produto.id}
+                    label={item.produto.nome} // Corrigido para item.produto.nome
+                    checked={selectedProdutos.includes(item.produto.id)}
+                    onChange={() => handleProductSelect(item.produto.id)}
+                />
+            </div>
+        ))}
+</div>
+
+
+                        <CustomButton variant="primary" onClick={prevStep}>
+                            Voltar
+                        </CustomButton>
+                        <CustomButton variant="success" onClick={nextStep}>
+                            Próximo
+                        </CustomButton>
                     </StepContainer>
-                </Modal.Body>
-                <Modal.Footer>
-                    <CustomButton variant="secondary" onClick={handleClose}>
-                        Fechar
-                    </CustomButton>
-                    <CustomButton variant="primary" onClick={nextStep}>
-                        Confirmar
-                    </CustomButton>
-                </Modal.Footer>
-            </StyledModal>
-        </>
+                )}
+
+
+            </Modal.Body>
+        </StyledModal>
     );
 };
 
